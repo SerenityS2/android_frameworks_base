ee054bb704de0e7c840998ea5e7c79f9cbe5f82f
neighbors28 <neighbors28@naver.com>
2013-08-08 13:15:28 +0900

    Add Quick Peek Value

--- core/java/android/app/Activity.java
+++ core/java/android/app/Activity.java
@@ -34,6 +34,7 @@ import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
+import android.provider.Settings;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
@@ -49,19 +50,24 @@ import android.os.Parcelable;
 import android.os.RemoteException;
 import android.os.StrictMode;
 import android.os.UserHandle;
+import android.provider.Settings; 
 import android.text.Selection;
 import android.text.SpannableStringBuilder;
 import android.text.TextUtils;
 import android.text.method.TextKeyListener;
 import android.util.AttributeSet;
 import android.util.EventLog;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.util.TypedValue;  
 import android.util.Slog;
 import android.util.SparseArray;
 import android.view.ActionMode;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.ContextThemeWrapper;
+import android.view.Display;
+import android.view.Gravity;  
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -78,6 +84,7 @@ import android.view.WindowManager;
 import android.view.WindowManagerGlobal;
 import android.view.accessibility.AccessibilityEvent;
 import android.widget.AdapterView;
+import android.widget.Toast;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
@@ -715,6 +722,9 @@ public class Activity extends ContextThemeWrapper
 
     private CharSequence mTitle;
     private int mTitleColor = 0;
+
+    private boolean mQuickPeekAction = false;
+    private float mQuickPeekInitialY; 
 
     final FragmentManagerImpl mFragments = new FragmentManagerImpl();
     final FragmentContainer mContainer = new FragmentContainer() {
@@ -2393,6 +2403,9 @@ public class Activity extends ContextThemeWrapper
         return onKeyShortcut(event.getKeyCode(), event);
     }
 
+    boolean mightBeMyGesture = false;
+    float tStatus;
+
     /**
      * Called to process touch screen events.  You can override this to
      * intercept all touch screen events before they are dispatched to the
@@ -2404,6 +2417,53 @@ public class Activity extends ContextThemeWrapper
      * @return boolean Return true if this event was consumed.
      */
     public boolean dispatchTouchEvent(MotionEvent ev) {
+	final int action = ev.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+		tStatus = ev.getY();
+		if (Settings.System.getInt(getContentResolver(),
+                    Settings.System.STATUSBAR_PEEK, 0) == 1) {
+                    if (tStatus < getStatusBarHeight()) {
+			mQuickPeekInitialY = ev.getY();
+                        mQuickPeekAction = true;
+                        mightBeMyGesture = true;
+                        return true;
+		    }	
+                }
+		onUserInteraction();
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+		if (!mQuickPeekAction) {
+                    break;
+                }
+                if (Math.abs(ev.getY() - mQuickPeekInitialY) > getStatusBarHeight()) {
+                        mQuickPeekAction = false;
+                }
+                if (mightBeMyGesture) {
+                    if(ev.getY() > tStatus) {
+                        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+                        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                        mHandler.postDelayed(new Runnable() {
+                                                 public void run() {
+                                                                           getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+                                                                           getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);     
+                                                 }
+                                                 
+                                                 },4000);
+                    }
+                    
+                    mightBeMyGesture = false;    
+                    return true;
+                }
+                break;
+
+            default:
+		mQuickPeekAction = false;
+                mightBeMyGesture = false;
+                break;
+        } 
+
         if (ev.getAction() == MotionEvent.ACTION_DOWN) {
             onUserInteraction();
         }
@@ -2413,6 +2473,10 @@ public class Activity extends ContextThemeWrapper
         return onTouchEvent(ev);
     }
     
+    public int getStatusBarHeight() {
+        return getResources().getDimensionPixelSize(com.android.internal.R.dimen.status_bar_height);
+    } 
+
     /**
      * Called to process trackball events.  You can override this to
      * intercept all trackball events before they are dispatched to the
