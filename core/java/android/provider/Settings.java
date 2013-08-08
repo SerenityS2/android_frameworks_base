2096971b20d2bf3bae12ed4d0ffc22b52855febb
Jinpyo-desktop <jinpyo-desktop@neighbors28.(none)>
2013-08-09 02:09:38 +0900

    HALO (1/2) & cleanup

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3543,7 +3543,25 @@ public final class Settings {
          *
          * @hide
          */
-        public static final String STATUSBAR_PEEK = "statusbar_peek";  
+        public static final String STATUSBAR_PEEK = "statusbar_peek";
+
+        /**
+         * HALO, should default to 0 (no, do not show)
+         * @hide
+         */
+        public static final String HALO_ACTIVE = "halo_active";
+
+        /**
+         * HALO reversed?, should default to 1 (yes, reverse)
+         * @hide
+         */
+        public static final String HALO_REVERSED = "halo_reversed";
+
+        /**
+         * HALO hide?, should default to 0 (no, do not hide)
+         * @hide
+         */
+        public static final String HALO_HIDE = "halo_hide";
 
     }
 
