7e74c458319c33c81d3dd43ab214e97725480914
Lok-Hyung Son <Sleepy@tcpdev.kr>
2013-09-14 09:37:03 +0900

    Revert "App sidebar [1/2]"
    This reverts commit 288f07d6574dc27ba320d39413dbca807ebfdf36.

--- packages/SystemUI/src/com/android/systemui/statusbar/tablet/TabletStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/tablet/TabletStatusBar.java
@@ -678,11 +678,6 @@ public class TabletStatusBar extends BaseStatusBar implements
         filter.addAction(Intent.ACTION_SCREEN_OFF);
         context.registerReceiver(mBroadcastReceiver, filter);
 
-	if (mRecreating) {
-            removeSidebarView();
-        }
-        addSidebarView();
-
         return sb;
     }
 
