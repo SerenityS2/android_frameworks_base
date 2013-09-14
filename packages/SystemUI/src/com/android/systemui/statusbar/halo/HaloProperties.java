15f8797d1a81bdddd7206701a578d96528fafb49
MuJiGae <wfmtooopa@gmail.com>
2013-09-14 15:52:17 +0900

    Build for CyanogenMod

--- packages/SystemUI/src/com/android/systemui/statusbar/halo/HaloProperties.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/halo/HaloProperties.java
@@ -1,18 +1,18 @@
 /*
-* Copyright (C) 2013 ParanoidAndroid.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2013 ParanoidAndroid.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.android.systemui.statusbar.halo;
 
@@ -89,7 +89,7 @@ public class HaloProperties extends FrameLayout implements BatteryStateChangeCal
     private int mHaloContentHeight = 0;
     private int mMsgCount, mValue;
     private int mBatteryLevel = 0;
-    private boolean mCharging = false;
+    private int mCharging = 0;
     private boolean airPlaneMode;
 
     private boolean mConnected = true;
@@ -242,7 +242,7 @@ public class HaloProperties extends FrameLayout implements BatteryStateChangeCal
     }
 
     public int getHaloX() {
-        return mHaloX;
+        return mHaloX; 
     }
 
     public int getHaloY() {
@@ -492,16 +492,16 @@ public class HaloProperties extends FrameLayout implements BatteryStateChangeCal
     }
 
     @Override
-    public void onBatteryLevelChanged(int level, boolean pluggedIn) {
+    public void onBatteryLevelChanged(int level, int status) {
         mBatteryLevel = level;
-        mCharging = pluggedIn;
+        mCharging = status;
     }
 
     private BroadcastReceiver mBatteryReceiver = new BroadcastReceiver(){
         @Override
         public void onReceive(Context arg0, Intent intent) {
             mBatteryLevel = intent.getIntExtra("level", 0);
-            mCharging = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, 0) != 0;
+            mCharging = (intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, 0) != 0) ? 1 : 0;
         }
     };
 
@@ -510,7 +510,8 @@ public class HaloProperties extends FrameLayout implements BatteryStateChangeCal
     }
 
     public boolean getBatteryStatus() {
-        return mCharging;
+        if (mCharging == 1) return true;
+        return false;
     }
 
     @Override
